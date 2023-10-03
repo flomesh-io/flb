aa1225798f534197ed583fba3f83ac50b23eab60
Cybwan <baili@flomesh.io>
2023-10-03 12:02:52 +0800

    Code optimization

--- pkg/datapath/fw.go
+++ pkg/datapath/fw.go
@@ -2,9 +2,14 @@ package datapath
 
 /*
 #include <stdint.h>
-
+#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
+#include <stdbool.h>
+#include <string.h>
+#include <unistd.h>
 #include <sys/types.h>
-
+#include <linux/types.h>
 #include "../../ebpf/common/common_pdi.h"
 */
 import "C"
