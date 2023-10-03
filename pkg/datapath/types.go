a227b82df869b573468c2d3ea08cfa702120332c
Cybwan <baili@flomesh.io>
2023-10-03 12:37:34 +0800

    Code optimization

--- pkg/datapath/types.go
+++ pkg/datapath/types.go
@@ -10,14 +10,17 @@ package datapath
 #include <unistd.h>
 #include <sys/types.h>
 #include <linux/types.h>
+
 #include <bpf.h>
 #include <pdi.h>
 #include <flb_dpapi.h>
 #include <common_pdi.c>
+
 #cgo CFLAGS:  -I./../../ebpf/headers/linux -I./../../ebpf/common
 */
 import "C"
+import "fmt"
 
-var (
-	a C.struct_pdi_key
-)
+func Test() {
+	fmt.Println(C.sizeof_struct_dp_fwv4_ent)
+}
