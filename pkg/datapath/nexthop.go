9eadbd826afe3dbdeb35003ab8d55776a5c196ef
Cybwan <baili@flomesh.io>
2023-10-04 09:45:25 +0800

    Code optimization

--- pkg/datapath/nexthop.go
+++ pkg/datapath/nexthop.go
@@ -1,73 +1,77 @@
 package datapath
 
+/*
+#include <string.h>
+*/
+import "C"
 import (
-	"fmt"
-	"net"
 	"unsafe"
 
-	"github.com/flomesh-io/flb/pkg/consts"
-	"github.com/flomesh-io/flb/pkg/maps"
-	"github.com/flomesh-io/flb/pkg/maps/nh"
 	"github.com/flomesh-io/flb/pkg/tk"
 	. "github.com/flomesh-io/flb/pkg/wq"
 )
 
 // DpNextHopMod - routine to work on a ebpf next-hop change request
 func DpNextHopMod(w *NextHopDpWorkQ) int {
-	var act *maps.RtL2NhAct
-	var tunAct *maps.RtTunNhAct
+	var act *dp_rt_l2nh_act
+	var tunAct *dp_rt_tunnh_act
 
-	key := new(nh.Key)
-	key.NhNum = uint32(w.NextHopNum)
+	key := new(dp_nh_key)
+	key.nh_num = C.uint(w.NextHopNum)
 
 	if w.Work == DpCreate {
-		dat := new(nh.Act)
+		dat := new(dp_nh_tact)
+		C.memset(unsafe.Pointer(dat), 0, sizeof_struct_dp_nh_tact)
 		if !w.Resolved {
-			dat.Ca.ActType = consts.DP_SET_TOCP
+			dat.ca.act_type = DP_SET_TOCP
 		} else {
 			if w.TunNh {
-				fmt.Printf("Setting tunNh 0x%x\n", key.NhNum)
+				tk.LogIt(tk.LogDebug, "Setting tunNh 0x%x\n", key.nh_num)
 				if w.TunType == DpTunIPIP {
-					dat.Ca.ActType = consts.DP_SET_NEIGH_IPIP
+					dat.ca.act_type = DP_SET_NEIGH_IPIP
 				} else {
-					dat.Ca.ActType = consts.DP_SET_NEIGH_VXLAN
+					dat.ca.act_type = DP_SET_NEIGH_VXLAN
 				}
-				tunAct = (*maps.RtTunNhAct)(unsafe.Pointer(&dat.Anon0[0]))
+				tunAct = (*dp_rt_tunnh_act)(getPtrOffset(unsafe.Pointer(dat),
+					sizeof_struct_dp_cmn_act))
 
 				ipAddr := tk.IPtonl(w.RIP)
-				tunAct.L3t.RIp = ipAddr
-				tunAct.L3t.SIp = tk.IPtonl(w.SIP)
+				tunAct.l3t.rip = C.uint(ipAddr)
+				tunAct.l3t.sip = C.uint(tk.IPtonl(w.SIP))
 				tid := (w.TunID << 8) & 0xffffff00
-				tunAct.L3t.TId = tk.Htonl(tid)
+				tunAct.l3t.tid = C.uint(tk.Htonl(tid))
 
-				act = &tunAct.L2Nh
-				copy(act.Dmac[:], w.DstAddr[:])
-				copy(act.Smac[:], w.SrcAddr[:])
-				act.Bd = uint16(w.BD)
+				act = (*dp_rt_l2nh_act)(&tunAct.l2nh)
+				C.memcpy(unsafe.Pointer(&act.dmac[0]), unsafe.Pointer(&w.DstAddr[0]), 6)
+				C.memcpy(unsafe.Pointer(&act.smac[0]), unsafe.Pointer(&w.SrcAddr[0]), 6)
+				act.bd = C.ushort(w.BD)
 			} else {
-				dat.Ca.ActType = consts.DP_SET_NEIGH_L2
-				act = (*maps.RtL2NhAct)(unsafe.Pointer(&dat.Anon0[0]))
-				copy(act.Dmac[:], w.DstAddr[:])
-				copy(act.Smac[:], w.SrcAddr[:])
-				act.Bd = uint16(w.BD)
-				act.RnhNum = uint16(w.NNextHopNum)
+				dat.ca.act_type = DP_SET_NEIGH_L2
+				act = (*dp_rt_l2nh_act)(getPtrOffset(unsafe.Pointer(dat),
+					sizeof_struct_dp_cmn_act))
+				C.memcpy(unsafe.Pointer(&act.dmac[0]), unsafe.Pointer(&w.DstAddr[0]), 6)
+				C.memcpy(unsafe.Pointer(&act.smac[0]), unsafe.Pointer(&w.SrcAddr[0]), 6)
+				act.bd = C.ushort(w.BD)
+				act.rnh_num = C.ushort(w.NNextHopNum)
 			}
 		}
 
-		srcHwAddr := net.HardwareAddr(w.SrcAddr[:])
-		dstHwAddr := net.HardwareAddr(w.DstAddr[:])
-		err := llb_add_map_elem(consts.LL_DP_NH_MAP, key, dat)
-		if err != nil {
-			fmt.Printf("[DP] Nexthop %5d %s %s add[NOK] %x\n", w.NextHopNum, srcHwAddr.String(), dstHwAddr.String(), err)
-			return consts.EbpfErrNhAdd
+		sErr := llb_add_map_elem(LL_DP_NH_MAP,
+			unsafe.Pointer(key),
+			unsafe.Pointer(dat))
+		if sErr != nil {
+			return EbpfErrNhAdd
 		}
-		fmt.Printf("[DP] Nexthop %5d %s %s add[OK]\n", w.NextHopNum, srcHwAddr.String(), dstHwAddr.String())
 		return 0
 	} else if w.Work == DpRemove {
-		dat := new(nh.Act)
+		dat := new(dp_nh_tact)
+		C.memset(unsafe.Pointer(dat), 0, sizeof_struct_dp_nh_tact)
 		// eBPF array elements cant be deleted. Instead we just reset it
-		llb_add_map_elem(consts.LL_DP_NH_MAP, key, dat)
+		llb_add_map_elem(LL_DP_NH_MAP,
+			unsafe.Pointer(key),
+			unsafe.Pointer(dat))
 		return 0
 	}
-	return consts.EbpfErrWqUnk
+
+	return EbpfErrWqUnk
 }
