1ca4d8794cf17bfa202342b2cf63bf0581e1d8a2
Cybwan <baili@flomesh.io>
2023-10-05 12:46:15 +0800

    Code optimization

--- pkg/datapath/stats.go
+++ pkg/datapath/stats.go
@@ -3,13 +3,12 @@ package datapath
 import (
 	"github.com/flomesh-io/flb/internal"
 	"github.com/flomesh-io/flb/pkg/bpf"
-	"github.com/flomesh-io/flb/pkg/maps"
 )
 
 func clearStatsPcpuArr(mapName string, idx uint32) {
 	if nrCpus, err := internal.PossibleCPUs(); err == nil {
 		key := &idx
-		val := make([]maps.PbStats, nrCpus)
+		val := make([]PbStats, nrCpus)
 		bpf.UpdateMap(mapName, &key, &val)
 	}
 }
